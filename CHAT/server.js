5d0ff77623143659d34e9e8d81e5bcbac00adf8a
JoshCell <cellsius_bdn@hotmail.com>
2018-04-24 14:10:40 +0200

    Add files via upload

--- CHAT/server.js
+++ CHAT/server.js
@@ -1,126 +1,183 @@
-//Server.js
+/**
+ * Server.js
+ * @author : DiganmeGiovanni | https://twitter.com/DiganmeGiovanni
+ * @Created on: 25 Oct, 2014
+ */
 
-//@autor: josh cell
 
-var bodyParser = require('body-parser');
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+/* Librerias necesarias para la aplicación */
+var bodyParser  = require('body-parser');
+var express     = require('express');
+var app         = express();
+var http        = require('http').Server(app);
+var io          = require('socket.io')(http);
 var MongoClient = require('mongodb').MongoClient;
-var userDAO = require('./UserDAO').UserDAO;
+var userDAO     = require('./dao/UserDAO').UserDAO;
+var messageDAO  = require('./dao/messagesDAO').MessageDAO;
+var assert      = require('assert');
+//var db          = require('./config/db');
+//var mongoose    = require('mongoose');
 
-//Para acceder a los parametros de las peticiones POST
-
+// Para acceder a los parametros de las peticiones POST
 //app.use(bodyParser()); DEPRECATED
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-//Mongodb config
+/* Mongodb config */
 var mdbconf = {
-    host: 'localhost',
-    port: '27017',
-    db: 'warchat' //nombre del tutorial: chatSS
+  host: 'localhost',
+  port: '27017',
+  db: 'Warchat'
 };
 
-// Get a mondoDB connection and start application
+/* Get a mongodb connection and start application */
+var urlMongoDB = 'mongodb://'+mdbconf.host+':'+mdbconf.port;
+MongoClient.connect(urlMongoDB, function (err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  //if (err) return new Error('Connection to mongodb unsuccessful');
+  var db = client.db(mdbconf.db);
+  //
+  var usersDAO = new userDAO(db); // Initialize userDAO
+  var messagesDAO = new messageDAO(db);
+  
+  var onlineUsers = [];
+  //client.close();
+/** *** *** ***
+ *  Configuramos el sistema de ruteo para las peticiones web:
+ */
+  
+  app.get('/signup', function (req, res) {
+    res.sendFile( __dirname + '/Warchat/views/signup.html');
+  });
+  
+  app.post('/signup', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var email    = req.body.email;
+    
+    usersDAO.addUser(username, password, email, function (err, user) {
+      if (err) {
+        res.send({ 'error': true, 'err': err});
+      }
+      else {
+        //user.password = null;
+        res.send({ 'error': false, 'user': user });
+      }
+    });
+  });
 
-MongoClient.connect('mongodb://'+mdbconf.host+':'+mdbconf.port+'/'+mdbconf.db, function(err, db){
-   if(err) return new Error('Connection to mongodb unsuccessful');
+  app.post('/login', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
     
-    var usersDAO = new userDAO(db); //Inicializar userDAO
-    var onlineUsers = [];
+    usersDAO.validateLogin(username, password, function (err, user) {
+      if (err) {
+        res.send({'error': true, 'err': err});
+      }
+      else {
+        user.password = null;
+        res.send({ 'error': false, 'user': user});
+      }
+    });
+  });
+  
+  /** css and js request */
+  app.get('/css/foundation.min.css', function (req, res) {
+    res.sendFile(__dirname + '/Warchat/views/css/foundation.min.css');
+  });
+
+  app.get('/css/normalize.css', function (req, res) {
+    res.sendFile(__dirname + '/Warchat/views/css/normalize.css');
+  });
+  
+  app.get('/js/foundation.min.js', function (req, res) {
+    res.sendFile(__dirname + '/Warchat/views/js/foundation.min.js');
+  });
+  /** *** *** */
     
-    //Configuramos el sistema de ruteo para las peticiones web
+    /*app.get('/js/chat.js', function (req, res){
+        res.sendFile(__dirname + 'Warchat/views/js/chat.js');
+    });*/
     
-    app.get('/signup', function(req, res){
-        res.sendFile(__dirname + '/Chat-SS/views/signup.html');
+    /*app.get('/js/moment-width-locales.min.js', function(req, res){
+        res.sendFile(__dirname + '/Warchat/views/js/moment-with-locales.min.js');
+    });*/
+    
+  /** *** *** */
+  
+  app.get('*', function(req, res) {
+    res.sendFile( __dirname + '/Warchat/views/chat.html');
+  });
+
+
+  /** *** *** ***
+   *  Configuramos Socket.IO para estar a la escucha de
+   *  nuevas conexiones. 
+   */
+  io.on('connection', function(socket) {
+    
+    console.log('New user connected');
+    
+    /**
+     * Cada nuevo cliente solicita con este evento la lista
+     * de usuarios conectados en el momento.
+     */
+    socket.on('all online users', function () {
+      socket.emit('all online users', onlineUsers);
     });
     
-    app.post('/signup', function(req, res){
-        var username = req.body.username;
-        var password = req.body.password;
-        var email = req.body.email;
-        
-        usersDAO.addUser(username, password, email, function (err, user) {
-            if (err) {
-                res.send({ 'error': true, 'err': err});
-            }else{
-                user.password = null;
-                res.send({ 'error': false, 'user': user});
-            }
-        });
-    });
-    
-    app.post('/login', function (req, res){
-        var username = req.body.username;
-        var password = req.body.password;
-        
-        usersDAO.validateLogin(username, password, function (err, user){
-            if (err){
-                res.send({'error': true, 'err':err});
-            }else{
-                user.password = null;
-                res.send({ 'error': false, 'user': user});
-            }
-        });
-    });
-    
-    //css and js request
-    app.get('/css/foundation.min.css', function (req, res){
-        res.sendFile(__dirname + '/Chat-SS/views/css/foundation.min.css');
-    });
-    
-    app.get('/css/normalize.css', function (req, res){
-        res.sendFile(__dirname + '/Chat-SS/views/css/normalize.css');
-    });
-    
-    app.get('/css/foundation.min.js', function (req, res){
-        res.sendFile(__dirname + '/Chat-SS/views/js/foundation.min.js');
-    });
-    
-    app.get('*', function(req, res){
-        res.sendFile(__dirname + '/Chat-SS/views/chat.html');
-    });
-
-
-    //Configuramos SOcket.IO para estar a la escucha de nuevas conexiones.
-
-    io.on('connection', function(socket){
-        console.log('New user connected');
-
-        //Cada nuevo cliente solicita con este evento la lista de usuarios conectados en el momento.
-
-        socket.on('all online users', function(){
-            socket.emit('all online users', onlineUsers);
-        });
-
-        //Cada nuevo socket deberá estar a la escucha del evento 'chat message', el cual se activa cada vez que un usuario envia un mensaje.
-
-        socket.on('chat message', function(msg){
+    /**
+     * Cada nuevo socket debera estar a la escucha
+     * del evento 'chat message', el cual se activa
+     * cada vez que un usuario envia un mensaje.
+     * 
+     * @param  msg : Los datos enviados desde el cliente a 
+     *               través del socket.
+     */
+    socket.on('chat message', function(msg) {
+        messagesDAO.addMessage(msg.username, Date.now(), msg.message, function (err, nmsg){
             io.emit('chat message', msg);
-        });
-
-        //Mostramos en consola cada vez que un usuario se desconecte del sistema.
-
-        socket.on('disconnect', function(){
-            onlineUsers.splice(onlineUsers.indexOf(socket.user), 1);
-            io.emit('remove user', socket.user);
-            console.log('User disconnected');
-        });
-
-        socket.on('new user', function (nuser){
-            socket.user = nuser;
-            onlineUsers.push(nuser);
-            io.emit('new user', nuser);
+        })
+    });
+      
+    socket.on('latest messages', function(){
+        messagesDAO.getLatest(50, function (err, messages){
+            if (err) console.log('Error getting messages from history');
+            socket.emit('latest messages', messages);
         });
     });
-
-//Iniciamos la aplicación en el puerto 3000
-
-    http.listen(3000, function(){
-        console.log('listening on *:3000');
+    
+    /**
+     * Mostramos en consola cada vez que un usuario
+     * se desconecte del sistema.
+     */
+    socket.on('disconnect', function() {
+      onlineUsers.splice(onlineUsers.indexOf(socket.user), 1);
+      io.emit('remove user', socket.user);
+      console.log('User disconnected');
     });
+    
+    /**
+     * Cuando un cliente se conecta, emite este evento
+     * para informar al resto de usuarios que se ha conectado.
+     * @param  {[type]} nuser El nuevo usuarios
+     */
+    socket.on('new user', function (nuser) {
+      socket.user = nuser;
+      onlineUsers.push(nuser);
+      io.emit('new user', nuser);
+    });
+    
+  });
+
+
+  /**
+   * Iniciamos la aplicación en el puerto 3000
+   */
+  http.listen(3000, function() {
+    console.log('listening on *:3000');
+  });
 });
