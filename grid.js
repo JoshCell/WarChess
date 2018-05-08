9d8931b66a6a234e4d46d4abcc2a70c3b57e3c05
JoshCell <cellsius_bdn@hotmail.com>
2018-05-08 14:13:42 +0200

    Add files via upload

--- grid.js
+++ grid.js
@@ -38,8 +38,8 @@ var grid = d3.select("#grid")
             .append("svg")
             //.attr('xlink:href', "public/img/Cuadricula.svg")
             //.attr("viewBox", "-300,0,1500, 1500")
-            .attr("width", "1500px")
-            .attr("height", "1500px")
+            .attr("width", "770px")
+            .attr("height", "770px")
             .attr("x", "0px");
 
 ///////////SE CREAN LAS FILAS DEL TABLERO//////////////////////////////
@@ -53,8 +53,11 @@ var row = grid.selectAll(".row")
 
 var column = row.selectAll(".square")
 	.data(function(d) { return d; })
-	.enter().append("image")
-	.attr("class","square")
+	.enter().append("g")
+    .attr("class","square");
+    
+            
+    column.append("image")
 	.attr("x", function(d) { return d.x; })
 	.attr("y", function(d) { return d.y; })
 	.attr("width", function(d) { return d.width; })
@@ -63,9 +66,73 @@ var column = row.selectAll(".square")
 	//.style("fill", "#fff")
 	//.style("stroke", "#222")
 	.on('click', function(d) {
-       d.click++;
-       if ((d.click)%3 == 0 ) { d3.select(this).attr('xlink:href',"public/img/terreno01.png"); }
-	   if ((d.click)%3 == 1 ) { d3.select(this).attr('xlink:href',"public/img/terreno02.png"); }
-	   if ((d.click)%3 == 2 ) { d3.select(this).attr('xlink:href',"public/img/terreno03.png"); }
+        squareUnit.remove();
+        d.click++;
+        if(clickUni==false){
+            if ((d.click)%3 == 0 ) { d3.select(this).attr('xlink:href',"public/img/terreno01.png"); }
+	        if ((d.click)%3 == 1 ) { d3.select(this).attr('xlink:href',"public/img/terreno02.png"); }
+            if ((d.click)%3 == 2 ) { d3.select(this).attr('xlink:href',"public/img/terreno03.png"); }
+            
+        }
     });
 
+    var squareUnit = column.append("image")
+    .attr("x", function(d) { return d.x; })
+	.attr("y", function(d) { return d.y; })
+	.attr("width", function(d) { return d.width; })
+	.attr("height", function(d) { return d.height; })
+    .on('click', function(d){
+        d.click++;
+        if(clickUni==true){
+            if ((d.click)%2 == 0 ) { d3.select(this).attr('xlink:href',"public/img/infanteria01red.png"); }
+	        if ((d.click)%2 == 1 ) { d3.select(this).attr('xlink:href',"public/img/infanteria01blue.png"); }
+        }
+    })
+    
+    /*column.append("image")
+    .attr("x", function(d) { return d.x; })
+	.attr("y", function(d) { return d.y; })
+	.attr("width", function(d) { return d.width; })
+	.attr("height", function(d) { return d.height; })
+    .on("click", function(d){
+        if ((d.click)%2 == 0 ) { d3.select(this).append("image")
+                    .attr("x", function(d) { return d.x; })
+	                .attr("y", function(d) { return d.y; })
+                    .attr("width", function(d) { return d.width; })
+                    .attr("height", function(d) { return d.height; }).attr('xlink:href',"public/img/infanteria01red.png"); 
+            }
+	        if ((d.click)%2 == 1 ) { d3.select(this).append("image")
+                    .attr("x", function(d) { return d.x; })
+	                .attr("y", function(d) { return d.y; })
+                    .attr("width", function(d) { return d.width; })
+                    .attr("height", function(d) { return d.height; })
+                    .attr('xlink:href',"public/img/infanteria01blue.png"); };
+    });*/
+
+
+
+/////////////////////INCLUSIÓN DE UNIDADES/////////////////////////////////////////////////
+
+var bloqueBotones = d3.select("#grid")
+    .data(gridData)
+    .append("svg")
+    .attr("width", "50px")
+    .attr("height", "780px")
+    .attr("x", "200px")
+    .attr("y", "0px");
+    var clickUni = false;
+    bloqueBotones.append("image")
+        .attr("x", 5)
+        .attr("y", 10)
+        .attr("width", 20)
+        .attr("height", 20)
+        .attr('xlink:href', "public/img/IconoUnidades.png")
+        .on("click", function(d){
+            clickUni = clickUni == false ? true : false;
+            if(clickUni == true){
+                d3.select(this).attr('xlink:href',"public/img/IconoTerreno.png")
+            }else{
+                d3.select(this).attr('xlink:href',"public/img/IconoUnidades.png")
+            }
+            console.log(clickUni);
+        });
--- menuStats.js
+++ menuStats.js
@@ -7,8 +7,13 @@ function statsWindowGenerator(){
     var stswin = [{ "x": 20,"y": 75,"width": 250, "height": 200, "click": 0}]; return stswin;
 }
 
+function showStatsPartida(){
+    var statsP = [{ "RedTeam:": 0, "BlueTeam": 0, "RedPoints": 0, "BluePoints": 0}]; return statsP;
+}
+
 var buttonData = buttonGenerator();
 var statsWinData = statsWindowGenerator();
+var statsPartida = showStatsPartida();
 //console.log(menuData);
 
 var menu = d3.select("#menu")
@@ -20,20 +25,73 @@ var menu = d3.select("#menu")
             
 
 //////////////////////////VENTANA BOTONES///////////////////////
-
+var ButStaBol = false;
 var buttonStats = menu.selectAll(".buttonStats")
             .data(buttonData)
             .enter().append("g")
             .attr("class", "buttonStats");
-    buttonStats.append("rect")
+    var buttonImg = buttonStats.append("image")
+            .attr("class", "buttonSt")
+            .attr("width", function(d) { return d.width;})
+            .attr("height", function(d) { return d.height;})
+            .attr("x", function(d) { return d.x;})
+            .attr("y", function(d) { return d.y;})
+            .attr('xlink:href',"public/img/buttonStatsRed.png")
+            .on("mousedown", function(d){
+               ButStaBol = true; 
+                console.log(ButStaBol);
+                d3.select(this).attr('xlink:href',"public/img/buttonStatsGreen.png")})
+            .on("mouseup", function(d){
+               ButStaBol = false; 
+                console.log(ButStaBol);
+                d3.select(this).attr('xlink:href',"public/img/buttonStatsRed.png")});
+
+///////////////////////////////VENTANA STATS//////////////////////////////
+var statsWindow = menu.selectAll(".statsWindow")
+    .data(statsWinData)
+    .enter().append("g")
+    .attr("class", "statsWindow");
+    
+    var rectStatsWindow = statsWindow.append("rect")
+        .attr("class", "rectStatsWindow")
+        .attr("x", function(d) { return d.x;})
+        .attr("y", function(d) { return d.y;})
+        .attr("width", function(d) { return d.width;})
+        .attr("height", function(d) { return d.height;})
+        .style("fill", "white")
+        .style("stroke", "#222");
+ //  if(ButStaBol == true){
+        rectStatsWindow.append("text")
+            .data(statsPartida)
+            .attr("class", "stsPText")
+            //.attr("width", "100px")
+            //.attr("height", "100px")
+            //.attr("x", "5px")
+            //.attr("y", "5px")
+            .style("font-size", "10")
+            .style("fill", "red")
+            //.on("mousedown", function(d){
+            //d3.select(this).text("Equipo Rojo: " + "RedTeam", function(d) { return d.RedTeam;});
+            .text("Equipo Rojo: ");
+        //});
+            
+    //}
+    /*var buttonRectStats = buttonStats.append("rect")
             .attr("class", "buttonStatsRect")
             .attr("x", function(d) { return d.x;})
             .attr("y", function(d) { return d.y;})
             .attr("width", function(d) { return d.width;})
             .attr("height", function(d) { return d.height;})
             .style("fill", "red")
-            .style("stroke", "#222");
-            /*.on("click", function(d){
+            .style("stroke", "#222")
+            .on("mousedown", function(d){
+                d3.select(this).style("fill", "green")
+            })
+            .on("mouseup", function(d){
+                d3.select(this).style("fill", "red")
+            });
+            
+            .on("click", function(d){
             d.click++;
             if((d.click)%2==1){
                var windowStats = menu.select(".windowStats")
@@ -45,32 +103,20 @@ var buttonStats = menu.selectAll(".buttonStats")
               }else{
                   windowStats.close();
               }
-    });*/
+    });
 
-    buttonStats.append("text")
+    buttonRectStats.append("text")
             .text("STATS PARTIDA")
             .style("fill", "blue")
-            .style("font-size", "25")
-            .attr("x", function(d) { return d.x*2;})
-            .attr("y", function(d) { return d.y*2.75});
+            .style("font-size", "25");
+            //.attr("x", "5px")
+            //.attr("y", "5px");/*
 
 //////////////////////////VENTANA STATS/////////////////////
 
 //var stats = d3.select("#menu").select("svg");
 
-var statsWindow = menu.selectAll(".statsWindow")
-    .data(statsWinData)
-    .enter().append("g")
-    .attr("class", "statsWindow");
-    
-    statsWindow.append("rect")
-        .attr("class", "rectStatsWindow")
-        .attr("x", function(d) { return d.x;})
-        .attr("y", function(d) { return d.y;})
-        .attr("width", function(d) { return d.width;})
-        .attr("height", function(d) { return d.height;})
-        .style("fill", "white")
-        .style("stroke", "#222");
+
 /*
 var but = stats.selectAll(".buttons")
     .data(function(d){ return d; })
